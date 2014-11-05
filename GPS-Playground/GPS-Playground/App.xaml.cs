676f3b900033d2781e17f3c1490271466c663a29
Lucas <lucaswyland11@gmail.com>
2014-11-05 00:30:02 -0600

    Added GMap Control To Application

--- GPS-Playground/GPS-Playground/App.xaml.cs
+++ GPS-Playground/GPS-Playground/App.xaml.cs
@@ -13,5 +13,6 @@ namespace GPS_Playground
     /// </summary>
     public partial class App : Application
     {
+
     }
 }
--- GPS-Playground/GPS-Playground/MainWindow.xaml.cs
+++ GPS-Playground/GPS-Playground/MainWindow.xaml.cs
@@ -1,28 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
+using GMap.NET;
+using GMap.NET.WindowsPresentation;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
 
 namespace GPS_Playground
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            // Switch to Cache mode if internet is not avaible
+            if (!PingNetwork("pingtest.com"))
+            {
+                MainMap.Manager.Mode = AccessMode.CacheOnly;
+                MessageBox.Show("No internet connection available, going to CacheOnly mode.", "GMap.NET - Demo.WindowsPresentation", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+
+        }
+
+
+        // TODO: DOES NOT BELONG HERE
+        public static bool PingNetwork(string hostNameOrAddress)
+        {
+            bool pingStatus = false;
+
+            using (Ping p = new Ping())
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+                int timeout = 4444; // 4s
+
+                try
+                {
+                    PingReply reply = p.Send(hostNameOrAddress, timeout, buffer);
+                    pingStatus = (reply.Status == IPStatus.Success);
+                }
+                catch (Exception)
+                {
+                    pingStatus = false;
+                }
+            }
+
+            return pingStatus;
         }
     }
 }
